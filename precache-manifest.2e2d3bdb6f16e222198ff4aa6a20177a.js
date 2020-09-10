d01701cc1d14f0c3e2f2cab6c66be6495d8e8d2f
GeniusHawlah <geniusdecode@gmail.com>
2020-09-10 16:32:24 +0100

    Initial commit

---
+++ precache-manifest.2e2d3bdb6f16e222198ff4aa6a20177a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8da65fde19b849c0f9abb193057782f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bd85bb4519be3173956",
+    "url": "/static/css/main.cbbbe0e2.chunk.css"
+  },
+  {
+    "revision": "dec90887f527117caf69",
+    "url": "/static/js/2.6bf0b6e6.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/static/js/2.6bf0b6e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bd85bb4519be3173956",
+    "url": "/static/js/main.06456645.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "6e1bcd618cd34d22a07dca2e052a769e",
+    "url": "/static/media/CGPA.6e1bcd61.jpg"
+  },
+  {
+    "revision": "e6e41b3ee093f1c171e77144214fa269",
+    "url": "/static/media/FirebaseLogin.e6e41b3e.jpg"
+  },
+  {
+    "revision": "a8d2f55fafe6e79970fc91b4f6106a4e",
+    "url": "/static/media/MyPortfolio.a8d2f55f.jpg"
+  },
+  {
+    "revision": "5202301f2407602af781663a7af132e7",
+    "url": "/static/media/ProjectN.5202301f.png"
+  },
+  {
+    "revision": "5d8a5e687796a569866894780412a5ec",
+    "url": "/static/media/TechReach.5d8a5e68.jpg"
+  },
+  {
+    "revision": "70ff37ba1cbf3c7f2541ebbe28fde911",
+    "url": "/static/media/bg3.70ff37ba.jpg"
+  },
+  {
+    "revision": "76622cbccd8a0fe79bad962c85b01025",
+    "url": "/static/media/profilePic.76622cbc.jpg"
+  }
+]);
