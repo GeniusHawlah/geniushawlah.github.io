e2055e6f772e7097fd94129e60c96fd15ed7372c
Olasunkanmi Ajibola <geniusdecode@gmail.com>
2020-12-05 16:33:52 +0100

    Changed the fav icon

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2e2d3bdb6f16e222198ff4aa6a20177a.js"
+  "/precache-manifest.2b30ff189b88e8bebe6f571ec0fa564d.js"
 );
 
 self.addEventListener('message', (event) => {
